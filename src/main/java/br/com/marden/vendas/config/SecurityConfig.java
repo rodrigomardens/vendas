a6b09556a8e11241d3b741f5c4218774d832d433
Rodrigo Marden <rodrigomardens@gmail.com>
2022-03-12 16:13:25 -0300

    Configurando roles e authorities

--- src/main/java/br/com/marden/vendas/config/SecurityConfig.java
+++ src/main/java/br/com/marden/vendas/config/SecurityConfig.java
@@ -19,12 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-            .withUser("fulano").password(passwordEncoder().encode("123"))
-            .roles("USER");
+                .withUser("fulano").password(passwordEncoder().encode("123"))
+                .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/clientes/**").hasAnyRole("ADMIN", "USER")
+                    .antMatchers("/api/pedidos/**").hasAnyRole("ADMIN", "USER")
+                    .antMatchers("/api/produtos/**").hasAnyRole("ADMIN")
+                .and()
+                .formLogin();
     }
 }
